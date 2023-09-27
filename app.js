e044924779344c6480ea4e60978c1e0914027f6d
Rob Elliott <elliott@iu.edu>
2023-09-27 14:35:24 -0400

    End of v2

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
+require('dotenv').config();
+
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const mongo = require('./services/database');
 
 // Routers
 const indexRouter = require('./routes/index');
 
 // App initialization
 const app = express();
+
+// Database initialization
+async function startDB() {
+  await mongo.init();
+}
+startDB();
 
 // View engine setup
 app.set('views', path.join(__dirname, 'views'));
